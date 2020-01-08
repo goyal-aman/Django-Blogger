fca7cce723af53c9b6f8eaefd4cef7e60c47b561
Aman Goyal <amangoyal8110@gmail.com>
2020-01-08 23:43:18 +0530

    added login/logout and profile page

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
-    path('register/', views.register, name='users-register')
+    path('register/', views.register, name='users-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='users-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='users-logout'),
+    path('profile/', views.profile, name='users-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -9,7 +10,11 @@ def register(request):
             form.save()
             username    = form.cleaned_data.get('username')
             messages.success(request,f'{username} Registered')
-            return redirect('blog-home')
+            return redirect('users-login')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', { 'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
