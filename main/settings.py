f06994c62cbd42de307e51242ac5c08b83c74605
Aman Goyal <amangoyal8110@gmail.com>
2020-01-09 17:58:42 +0530

    implemented signals

--- main/settings.py
+++ main/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crispy_forms',
-    'blog',
-    'users',
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
