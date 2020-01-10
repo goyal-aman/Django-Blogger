3e6ccdc1d7a4bf4cf24cb070ab41f9595ac3c000
Aman Goyal <amangoyal8110@gmail.com>
2020-01-11 00:16:12 +0530

    implemented pagination

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>/.html
     context_object_name = 'posts'
     ordering = ['-date_update']
+    paginate_by = 2 
 
 class PostDetailView(DetailView):
     model = Post
