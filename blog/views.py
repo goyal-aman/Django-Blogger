25a06883ae015f3b1c62f70d2a9e53da31903575
Aman Goyal <amangoyal8110@gmail.com>
2020-01-18 18:01:43 +0530

    fixed : follower and following bug, added details for cbv

--- blog/views.py
+++ blog/views.py
@@ -7,63 +7,87 @@ from django.contrib.auth import get_user_model
 from users.models import Profile
 # Create your views here.
 
+
 class UserProfileListView(ListView):
     '''
-    modified get_queryset, return only required user object
-    kwarg: username
+    urlconf name: 'blog-post-user'
+    url-parameter: username
+
+    modified get_queryset, return only required user post object
     '''
     model = Post
-    template_name = 'blog/profile_list.html'
+    template_name = 'blog/post_user.html'
     context_object_name = 'object'
     paginate_by = 10
-    
-
-    # def get_user(self, **kwargs):
-        # '''return user object with username = self.kwargs.get('username')'''
-        # return get_user_model().objects.get(username=self.kwargs.get('username'))
 
     def is_following(self, **kwargs):
         return self.request.user.profile.isfollowing.filter(user__username=self.kwargs.get('username')).count() == 1
-    
+
     def get_queryset(self):
-        self.profile_user = get_object_or_404(get_user_model(), username=self.kwargs.get('username'))
+        self.profile_user = get_object_or_404(
+            get_user_model(), username=self.kwargs.get('username'))
         return Post.objects.filter(author=self.profile_user)
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super(UserProfileListView, self).get_context_data(**kwargs)
-        context['user_object'] = get_user_model().objects.get(username=self.kwargs.get('username'))
-        # context['if_following'] = self.request.user.profile.isfollowing.filter(user__username=self.kwargs.get('username')).count() == 1
-        print('this is context', context)
+        context['user_object'] = get_user_model().objects.get(
+            username=self.kwargs.get('username'))
         return context
 
 
-class PostListView(LoginRequiredMixin  ,ListView):
+class PostListView(LoginRequiredMixin, ListView):
+    '''
+    urlconf name: 'blog-home'
+
+
+    updated get_queryset to contain logged in user post
+    '''
     model = Post
-    template_name = 'blog/home.html' #<app>/<model>_<viewtype>/.html
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>/.html
     context_object_name = 'posts'
     ordering = ['-date_update']
-    paginate_by = 10 
+    paginate_by = 10
 
     def get_queryset(self):
         user_post = Post.objects.filter(author=self.request.user)
-        qs = Post.objects.filter(author__profile__followers=self.request.user.profile)  #.order_by('-date_update')
+        # .order_by('-date_update')
+        qs = Post.objects.filter(
+            author__profile__followers=self.request.user.profile)
         return qs.union(user_post).order_by('-date_update')
 
 
 class PostDetailView(DetailView):
+    '''
+    urlconf name: 'blog-post-detail'
+    url-parameter: post.pk
+    template : 'post_detail.html'
+    '''
     model = Post
 
-class PostCreateView(LoginRequiredMixin ,CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    '''
+    urlconf : 'blog-post-create'
+    template used : post_form.html
+    '''
     model = Post
-    fields = ['title', 'content'] # <app>/<model>_form.html
+    fields = ['title', 'content']  # <app>/<model>_form.html
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    '''
+    urlconf : 'blog-post-update'
+    url parameter : post.pk
+
+    template used : post_form.html
+    '''
+
     model = Post
-    fields = ['title', 'content'] # <app>/<model>_form.html
+    fields = ['title', 'content']  # <app>/<model>_form.html
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -76,7 +100,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
 
-class PostDeleteView( LoginRequiredMixin, UserPassesTestMixin , DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    '''
+    urlconf : 'blog-post-delete'
+    url parameter : post.pk
+
+    template used : 'post_confirm_delete.html'
+    '''
+    
     model = Post
     success_url = '/'
 
@@ -84,19 +116,21 @@ class PostDeleteView( LoginRequiredMixin, UserPassesTestMixin , DeleteView):
         post = self.get_object()
         if self.request.user == post.author:
             return True
-    
+
+
 def show_followers(request, **kwargs):
+    user = get_user_model().objects.get(username=kwargs.get('username'))
     context = {
 
-        'followers' : request.user.profile.followedby.all()
+        'followers': user.profile.followedby.all()
     }
     return render(request, 'blog/followers.html', context)
 
 
 def show_following(request, **kwargs):
+    user = get_user_model().objects.get(username=kwargs.get('username'))
     context = {
 
-        'followings' : request.user.profile.isfollowing.all()
+        'followings': user.profile.isfollowing.all()
     }
     return render(request, 'blog/following.html', context)
-
