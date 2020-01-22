167eea1fd6241ba974b453d9532224d45fe40544
Aman Goyal <amangoyal8110@gmail.com>
2020-01-22 21:25:27 +0530

    updated STATIC_ROOT

--- main/settings.py
+++ main/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL              = '/static/'
+STATIC_ROOT              = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK    = 'bootstrap4'
 LOGIN_REDIRECT_URL      = 'blog-home'
 LOGIN_URL               = 'users-login'
