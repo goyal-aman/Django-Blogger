1ff6d3744eb1547f1f14be715d1abb72944b039a
Aman Goyal <amangoyal8110@gmail.com>
2020-01-24 01:15:31 +0530

    added: auto follow superuser

--- users/signals.py
+++ users/signals.py
@@ -6,8 +6,15 @@ from .models import Profile
 @receiver(post_save, sender=get_user_model())
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        new_user_profile = Profile.objects.create(user=instance)
 
+        #assuming there is single super user.
+        try:
+            superuser = get_user_model().objects.get(is_superuser=True)
+            superuser.profile.followedby.add(new_user_profile)
+            new_user_profile.isfollowing.add(superuser.profile)
+        except:
+            pass
 # @receiver(post_save, sender=get_user_model())
 # def save_profile(sender, instance, **kwargs):
 #     instance.profile.save()
